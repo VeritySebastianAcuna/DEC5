cf6f53338a613ff6454193fe32b10a4914fb6991
laura Andrade <laura.andrade@acepta.com>
2021-05-04 17:29:32 -0400

    Script 1067, 1070, 1071 clase Mis Documentos

--- src/test/java/test/Test_MisDocumentos.java
+++ src/test/java/test/Test_MisDocumentos.java
@@ -15,6 +15,7 @@ import org.testng.annotations.Test;
 
 import common.Configuration;
 import common.LeerExcel;
+import common.Log;
 import evidence.CrearLogyDocumento;
 import pages.PageAcepta;
 import pages.PageAvanzarPagina;
@@ -25,6 +26,7 @@ import pages.PageDec5;
 import pages.PageDescargarArchivos;
 import pages.PageLoginAdm;
 import pages.PageMisDocumentos;
+import pages.PagePendientes;
 import pages.PageSubirArchivos;
 
 public class Test_MisDocumentos {
@@ -63,18 +65,186 @@ public class Test_MisDocumentos {
 		pageDec5.OpcionUserName(cp);
 		pageAcepta.empresaAcepta(cp);
 		pageMisDocumentos.MisDocumentos(cp);
-		pageMisDocumentos.etiquetaDocumento(cp, datos[15]);
+		pageMisDocumentos.BusquedaEtiquetas(cp, datos[3]);
 		
-		String buscar=datos[15];
-		String resultado = pageMisDocumentos.ResultadoBusqueda(cp);
-		if(resultado.contains(buscar)) {
-			System.out.println("OK");
-			crearLogyDocumento.CasoOk(cp);
+		
+		if(driver.findElement(By.xpath("//*[@id=\"table-documentos\"]/tbody/tr/td[2]")).getText().contains(datos[3])) {
+			String texto = "Busqueda OK"; 
+			crearLogyDocumento.AgregarRegistroLog(cp, texto);
 		}
+		else {
+			crearLogyDocumento.CasoNok(cp);
+		}
+		
+		System.out.println("FLUJO OK");
+	}
+	
+	@Test
+	public void Script_1063() throws InterruptedException, IOException, InvalidFormatException {
+		String cp = "DEC_1063";
+		System.out.println(cp);
+		
+		PageDec5 pageDec5 = new PageDec5(driver);
+		PageLoginAdm pageLoginAdm = new PageLoginAdm(driver);
+		PageAcepta pageAcepta = new PageAcepta(driver);
+		PageMisDocumentos pageMisDocumentos = new PageMisDocumentos(driver);
+		CrearLogyDocumento crearLogyDocumento = new CrearLogyDocumento(driver);
+		crearLogyDocumento.CrearEvidencias(cp);
+		
+		String[] datos = leerExcel.ObtenerDatosCP(datapool,cp);
+		
+		pageDec5.ClickIngresarLogin(cp);
+		pageLoginAdm.LoginIdentidadDigital(cp, datos[1], datos[2]);
+		pageDec5.OpcionUserName(cp);
+		pageAcepta.empresaAcepta(cp);
+		pageMisDocumentos.MisDocumentos(cp);
+		pageMisDocumentos.BusquedaEtiquetas(cp, datos[3]);
+		
+		String msjNoResultados = driver.findElement(By.id("table-documentos")).getText();
+		if(msjNoResultados.equals("No se encontraron resultados")) {
+			System.out.println("OK");
+			crearLogyDocumento.AgregarRegistroLog(cp, msjNoResultados );
+			crearLogyDocumento.CasoOk(cp);
+			}
 		else {
 			System.out.println("NOK");
 			crearLogyDocumento.CasoNok(cp);
 		}
+		
+		System.out.println("FLUJO OK");
+	}
+	
+	@Test
+	public void Script_1067() throws InterruptedException, IOException, InvalidFormatException {
+		String cp = "DEC_1067";
+		System.out.println(cp);
+		
+		PageDec5 pageDec5 = new PageDec5(driver);
+		PageLoginAdm pageLoginAdm = new PageLoginAdm(driver);
+		PageAcepta pageAcepta = new PageAcepta(driver);
+		PageMisDocumentos pageMisDocumentos = new PageMisDocumentos(driver);
+		CrearLogyDocumento crearLogyDocumento = new CrearLogyDocumento(driver);
+		crearLogyDocumento.CrearEvidencias(cp);
+		
+		String[] datos = leerExcel.ObtenerDatosCP(datapool,cp);
+		
+		pageDec5.ClickIngresarLogin(cp);
+		pageLoginAdm.LoginIdentidadDigital(cp, datos[1], datos[2]);
+		pageDec5.OpcionUserName(cp);
+		pageAcepta.empresaAcepta(cp);
+		pageMisDocumentos.MisDocumentos(cp);
+		pageMisDocumentos.clickCarpeta(cp);
+		pageMisDocumentos.clickNuevaCarpeta(cp);
+		pageMisDocumentos.AgregarCarpetaYaExistente(cp);
+		
+		if(driver.findElement(By.xpath("//*[@id=\"modal\"]/div/div/form/div[2]/div/div/div/span")).getText().equals("Carpeta ya existe")) {
+			crearLogyDocumento.CasoOk(cp);
+		}
+		else {
+			crearLogyDocumento.CasoNok(cp);
+		}
+		
+		System.out.println("FLUJO OK");
+	}
+	
+	@Test
+	public void Script_1070() throws InterruptedException, IOException, InvalidFormatException {
+		String cp = "DEC_1070";
+		System.out.println(cp);
+		
+		PageDec5 pageDec5 = new PageDec5(driver);
+		PageLoginAdm pageLoginAdm = new PageLoginAdm(driver);
+		PageAcepta pageAcepta = new PageAcepta(driver);
+		PageMisDocumentos pageMisDocumentos = new PageMisDocumentos(driver);
+		CrearLogyDocumento crearLogyDocumento = new CrearLogyDocumento(driver);
+		crearLogyDocumento.CrearEvidencias(cp);
+		
+		String[] datos = leerExcel.ObtenerDatosCP(datapool,cp);
+		
+		pageDec5.ClickIngresarLogin(cp);
+		pageLoginAdm.LoginIdentidadDigital(cp, datos[1], datos[2]);
+		pageDec5.OpcionUserName(cp);
+		pageAcepta.empresaAcepta(cp);
+		pageMisDocumentos.MisDocumentos(cp);
+		pageMisDocumentos.clickCarpeta(cp);
+		pageMisDocumentos.FiltrarCarpetaExistente(cp);
+				
+	
+		
+		System.out.println("FLUJO OK");
+	}
+	
+	@Test
+	public void Script_1071() throws InterruptedException, IOException, InvalidFormatException {
+		String cp = "DEC_1071";
+		System.out.println(cp);
+		
+		PageDec5 pageDec5 = new PageDec5(driver);
+		PageLoginAdm pageLoginAdm = new PageLoginAdm(driver);
+		PageAcepta pageAcepta = new PageAcepta(driver);
+		PageMisDocumentos pageMisDocumentos = new PageMisDocumentos(driver);
+		CrearLogyDocumento crearLogyDocumento = new CrearLogyDocumento(driver);
+		crearLogyDocumento.CrearEvidencias(cp);
+		
+		String[] datos = leerExcel.ObtenerDatosCP(datapool,cp);
+		
+		pageDec5.ClickIngresarLogin(cp);
+		pageLoginAdm.LoginIdentidadDigital(cp, datos[1], datos[2]);
+		pageDec5.OpcionUserName(cp);
+		pageAcepta.empresaAcepta(cp);
+		pageMisDocumentos.MisDocumentos(cp);
+		pageMisDocumentos.clickCarpeta(cp);
+		pageMisDocumentos.EliminarCarpetaExistente(cp);
+		
+			String mensaje = driver.findElement(By.xpath("//*[@id=\"modalDeleteFolders\"]/div/div/div[1]/h1")).getText();
+			if(mensaje.equals("¿Estás seguro de querer eliminar la siguente carpeta?")) {
+				String texto1 = "Mensaje eliminar OK";
+				crearLogyDocumento.AgregarRegistroLog(cp, texto1);
+				System.out.println("Paso 2");
+				String btn = driver.findElement(By.xpath("//*[@id=\"eliminarFolder\"]")).getText();
+				if(btn.equals("Eliminar")) {
+					String texto2 = "Botón eliminar OK";
+					crearLogyDocumento.AgregarRegistroLog(cp, texto2);
+					System.out.println("Paso 3");
+					String carpeta = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div[2]/div/div/div[1]/div[4]/button")).getText();
+					if(driver.findElement(By.xpath("//*[@id=\"modalDeleteFolders\"]/div/div/div[2]")).getText().equals(carpeta)){
+						String texto = "Carpeta a eliminar OK";
+						crearLogyDocumento.AgregarRegistroLog(cp, texto);
+						System.out.println("Paso 1");
+					}
+				}
+			}
+		else {
+			crearLogyDocumento.CasoNok(cp);
+		}
+		
+		System.out.println("FLUJO OK");
+	}
+	
+	@Test
+	public void Script_1072() throws InterruptedException, IOException, InvalidFormatException {
+		String cp = "DEC_1072";
+		System.out.println(cp);
+		
+		PageDec5 pageDec5 = new PageDec5(driver);
+		PageLoginAdm pageLoginAdm = new PageLoginAdm(driver);
+		PageAcepta pageAcepta = new PageAcepta(driver);
+		PageMisDocumentos pageMisDocumentos = new PageMisDocumentos(driver);
+		CrearLogyDocumento crearLogyDocumento = new CrearLogyDocumento(driver);
+		crearLogyDocumento.CrearEvidencias(cp);
+		
+		String[] datos = leerExcel.ObtenerDatosCP(datapool,cp);
+		
+		pageDec5.ClickIngresarLogin(cp);
+		pageLoginAdm.LoginIdentidadDigital(cp, datos[1], datos[2]);
+		pageDec5.OpcionUserName(cp);
+		pageAcepta.empresaAcepta(cp);
+		pageMisDocumentos.MisDocumentos(cp);
+		pageMisDocumentos.clickCarpeta(cp);
+		pageMisDocumentos.EliminarCarpetaExistente(cp);
+		pageMisDocumentos.btnEliminarCarpeta(cp);
+		
+			
 		
 		System.out.println("FLUJO OK");
 	}
